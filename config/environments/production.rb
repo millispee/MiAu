65f62dbbae5c1cb537f2a7e203b1fee7a46bc9ee
Ludmila Spee <ludmilaspee@web.de>
2021-03-15 12:10:01 +0100

    added heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = {  host: 'https://hometog.herokuapp.com/' }
+  config.action_mailer.default_url_options = {  host: 'https://miaubooks.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
